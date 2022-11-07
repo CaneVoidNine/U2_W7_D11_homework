b543bd1bb8549f4c5f3e0cdc04dd8ca13e3e6f20
Damian Olszewski <damianolszewski25@gmail.com>
2022-11-07 22:20:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,64 @@ const loadFloyd = () => {
     })
     .catch((err) => console.error(err));
 };
+
+const loadMetal = () => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "7a3151f032msha018641da7ede56p1698ddjsn9683e54f257d",
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  };
+
+  fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=metallica", options)
+    .then((response) => response.json())
+    .then((songList) => {
+      let containerRow = document.querySelector(".container .row");
+      for (let i = 0; i < songList.data.length; i++) {
+        const song = songList.data[i];
+        const column = document.createElement("div");
+        column.className = "col-3";
+        column.innerHTML = `<div class="card" style="width: 18rem;">
+      <img src=${song.album.cover} class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text"> ${song.title} </p>
+      </div>
+    </div>`;
+        containerRow.appendChild(column);
+      }
+    })
+    .catch((err) => console.error(err));
+};
+
+const loadPunk = () => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "7a3151f032msha018641da7ede56p1698ddjsn9683e54f257d",
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  };
+
+  fetch(
+    "https://deezerdevs-deezer.p.rapidapi.com/search?q=daft%20punk",
+    options
+  )
+    .then((response) => response.json())
+    .then((songList) => {
+      let containerRow = document.querySelector(".container .row");
+      for (let i = 0; i < songList.data.length; i++) {
+        const song = songList.data[i];
+        const column = document.createElement("div");
+        column.className = "col-3";
+        column.innerHTML = `<div class="card" style="width: 18rem;">
+      <img src=${song.album.cover} class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text"> ${song.title} </p>
+      </div>
+    </div>`;
+        containerRow.appendChild(column);
+      }
+    })
+    .catch((err) => console.error(err));
+};
