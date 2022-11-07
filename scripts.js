fbe24b5f2a03607c702c31d9ad81e272fec2bdfc
Damian Olszewski <damianolszewski25@gmail.com>
2022-11-07 17:02:44 +0100

    not finished

--- scripts.js
+++ scripts.js
@@ -8,6 +8,10 @@ const options = {
 
 fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=pink floyd", options)
   .then((response) => response.json())
+
+  .then((data) => {
+    displayTitleFloyd(data);
+  })
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
 
@@ -20,3 +24,51 @@ fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=metallica", options)
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
+
+function displayTitleFloyd(data) {
+  const mainDiv = document.querySelector("#body");
+  for (i = 0; i < data.data.length; i++) {
+    let cardDiv = document.createElement("div");
+    cardDiv.className = "card";
+    cardDiv.style = "width: 18rem;";
+    let img = document.createElement("img");
+    img.src = data.data[i].album.cover_medium;
+    img.className = "card-img-top border border-danger";
+    cardDiv.appendChild(img);
+    let cardBodyDiv = document.createElement("div");
+    cardBodyDiv.className = "card-body border border-danger";
+    cardDiv.appendChild(cardBodyDiv);
+    let addH5 = document.createElement("h5");
+    addH5.className = "card-title";
+    addH5.innerText = data.data[i].title;
+    let addP = document.createElement("p");
+    addP.className = "card-text";
+    addP.innerText = "Song: " + data.data[i].title;
+
+    cardBodyDiv.appendChild(addP);
+    mainDiv.appendChild(cardDiv);
+  }
+  1000;
+}
+
+function displayTitleMetal(data) {
+  const mainDiv = document.querySelector("#butt");
+  for (i = 0; i < data.data.length; i++) {
+    let cardDiv = document.createElement("div");
+    cardDiv.className = "card";
+    cardDiv.style = "width: 18rem;";
+    let img = document.createElement("img");
+    img.src = data.data[i].album.cover_medium;
+    img.className = "card-img-top";
+    cardDiv.appendChild(img);
+    let cardBodyDiv = document.createElement("div");
+    cardBodyDiv.className = "card-body";
+    cardDiv.appendChild(cardBodyDiv);
+    let addH5 = document.createElement("h5");
+    addH5.className = "card-title m-5";
+    addH5.innerText = data.data[i].title;
+
+    mainDiv.appendChild(cardDiv);
+  }
+  1000;
+}
